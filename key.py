51bc8b7077220c6bc6cd4a6bac1c1b355a84ad19
cutrain <duanyuge@qq.com>
2019-06-16 22:57:37 +0800

    first commit

---
+++ key.py
@@ -0,0 +1,54 @@
+port = '8334'
+
+import keyboard as kb
+import zmq
+import pyautogui
+import time
+import queue
+import pickle
+import threading
+
+
+def kb_server():
+    global port
+    q = queue.Queue()
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind('tcp://*:'+port)
+    kb.start_recording(q)
+    while True:
+        a = q.get()
+        key_code = a.scan_code
+        name = a.name
+        event_type = a.event_type
+        message = pickle.dump(a)
+        print(a, key_code, name, event_type)
+        socket.send(message)
+        print('SEND', message)
+
+
+if __name__ == "__main__":
+    server_list = [kb_server]
+    args_list = [(,)]
+    if len(server_list) != len(args_list):
+        raise Exception("missing args for server")
+    thread_list = []
+    try:
+        for i in range(len(server_list)):
+            server = server_list[i]
+            args = args_list[i]
+            thread = threading.Thread(target=server, args=args)
+            thread.daemon = True
+            thread_list.append(thread)
+        for thread in thread_list:
+            thread.start()
+        print("Server start")
+        for thread in thread_list:
+            thread.join()
+    except KeyboardInterrupt:
+        thread_monitor = False
+        print("Server exiting, please wait")
+        for thread in thread_list:
+            thread.join()
+        print("Exit succeed")
+
